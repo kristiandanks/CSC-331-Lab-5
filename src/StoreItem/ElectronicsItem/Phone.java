11fce732ded70e42ce16942b5d74270a6ba71453
kristiandanks@gmail.com <kristiandanks@gmail.com>
2025-10-17 10:44:50 -0400

    Created Phone, TV and Laptop files, coded Phone class

---
+++ src/StoreItem/ElectronicsItem/Phone.java
@@ -0,0 +1,38 @@
+package StoreItem.ElectronicsItem;
+
+public class Phone extends ElectronicsItem{
+    //initializing variables
+    private int numOfCameras;
+
+    /**
+     * Constructor
+     * @param price double
+     * @param section String
+     * @param brand String
+     * @param name String
+     * @param returnPolicy String
+     * @param wattage int
+     * @param screenType String
+     * @param numOfCameras int
+     */
+    public Phone(double price,String section, String brand, String name, String returnPolicy,
+                 int wattage, String screenType, int numOfCameras){
+        super(price, section, brand, name, returnPolicy, wattage, screenType);
+        this.numOfCameras = numOfCameras;
+
+    }
+
+    //Getter
+    /**
+     *
+     * @return numOfCameras int
+     */
+    public int getNumOfCameras(){
+        return this.numOfCameras;
+    }
+
+    //Setter
+    public void setNumOfCameras(int numOfCameras){
+        this.numOfCameras = numOfCameras;
+    }
+}
