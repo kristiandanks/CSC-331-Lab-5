70cabb80c02f5230ccacd647ce8720a14f9ed6a0
Seacurs3 <seacurse@gmail.com>
2025-10-18 20:02:08 -0400

    Finished setting up the FoodItem objects

--- src/StoreItem/FoodItem/ShelfStable.java
+++ src/StoreItem/FoodItem/ShelfStable.java
@@ -1,4 +1,48 @@
 package StoreItem.FoodItem;
 
-public class ShelfStable {
+public class ShelfStable extends FoodItem {
+    //Initializing variable
+    private String boxedOrCanned;
+
+    /**
+     * Constructor
+     * @param price double
+     * @param section String
+     * @param brand String
+     * @param name String
+     * @param returnPolicy String
+     * @param expirationDate int
+     * @param organic boolean
+     * @param nutritionFacts String
+     * @param boxedOrCanned String
+     */
+
+    public ShelfStable(double price, String section, String brand, String name, String returnPolicy,
+                 int expirationDate, boolean organic, String nutritionFacts, String boxedOrCanned) {
+        super(price, section, brand, name, returnPolicy, expirationDate, organic, nutritionFacts);
+        this.boxedOrCanned = boxedOrCanned;
+    }
+
+    //Getter
+    /**
+     *
+     * @return boxedOrCanned String
+     */
+    public String getBoxedOrCanned(){
+        return this.boxedOrCanned;
+    }
+
+
+
+    //Setter
+
+    /**
+     *
+     * @param boxedOrCanned String
+     */
+    public void setBoxedOrCanned(String boxedOrCanned){
+        this.boxedOrCanned = boxedOrCanned;
+    }
+
+
 }
--- src/StoreItem/FoodItem/Vegetable.java
+++ src/StoreItem/FoodItem/Vegetable.java
@@ -1,4 +1,47 @@
 package StoreItem.FoodItem;
 
-public class Vegetable {
+public class Vegetable extends FoodItem {
+    //Initializing variable
+    private boolean isLeafy;
+
+    /**
+     * Constructor
+     * @param price double
+     * @param section String
+     * @param brand String
+     * @param name String
+     * @param returnPolicy String
+     * @param expirationDate int
+     * @param organic boolean
+     * @param nutritionFacts String
+     * @param isLeafy boolean
+     */
+
+    public Vegetable(double price, String section, String brand, String name, String returnPolicy,
+                 int expirationDate, boolean organic, String nutritionFacts, boolean isLeafy) {
+        super(price, section, brand, name, returnPolicy, expirationDate, organic, nutritionFacts);
+        this.isLeafy = isLeafy;
+    }
+
+    //Getter
+    /**
+     *
+     * @return isLeafy Boolean
+     */
+    public boolean getIsLeafy(){
+        return this.isLeafy;
+    }
+
+
+
+    //Setter
+
+    /**
+     *
+     * @param isLeafy boolean
+     */
+    public void setIsLeafy(boolean isLeafy){
+        this.isLeafy = isLeafy;
+    }
+
 }
